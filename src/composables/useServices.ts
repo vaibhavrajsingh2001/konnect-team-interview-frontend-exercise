ef1fbfde49a7414b0ba1b8f65874542edd726275
vaibhavrajsingh2001 <vaibhavrajsingh2001@gmail.com>
2024-02-15 20:36:09 +0530

    feat: add type to useServices composable

--- src/composables/useServices.ts
+++ src/composables/useServices.ts
@@ -1,39 +1,28 @@
 import { ref, onBeforeMount } from 'vue'
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
+import type { Service } from '@/types'
 
-// This composable is a simplified example for the exercise **and could likely be improved**.
-// Feel free to leave as-is, modify, or remove this file (and any others) as desired.
-// https://vuejs.org/guide/reusability/composables.html
+export default function useServices() {
+  const services = ref<Service[]>([])
+  const loading = ref<boolean>(false)
+  const error = ref<boolean>(false)
 
-export default function useServices(): any {
-  const services = ref<any[]>([])
-  const loading = ref<any>(false)
-  const error = ref<any>(false)
-
-  const getServices = async (): Promise<any> => {
+  const getServices = async (): Promise<void> => {
     try {
-      // Initialize loading state
       loading.value = true
 
-      // Fetch data from the API
-      const { data } = await axios.get('/api/services')
+      const response: AxiosResponse<Service[]> = await axios.get('/api/services')
 
-      // Store data in Vue ref
-      services.value = data
+      services.value = response.data
     } catch (err: any) {
       error.value = true
     } finally {
-      // Reset loading state
       loading.value = false
     }
   }
 
-  onBeforeMount(async (): Promise<void> => {
-    // Fetch services from the API
-    await getServices()
-  })
+  onBeforeMount(getServices)
 
-  // Return stateful data
   return {
     services,
     loading,
---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export interface Developer {
+  id: string;
+  name: string;
+  email: string;
+  avatar: string;
+}
+
+export interface Version {
+  id: string;
+  name: string;
+  description: string;
+  developer?: Developer;
+  updated_at: string;
+}
+
+export interface Metrics {
+  latency: number;
+  uptime: number;
+  requests: number;
+  errors: number;
+}
+
+export interface Service {
+  id: string;
+  name: string;
+  description: string;
+  type: 'REST' | 'HTTP';
+  published: boolean;
+  configured: boolean;
+  versions: Array<Version>;
+  metrics?: Metrics;
+}
